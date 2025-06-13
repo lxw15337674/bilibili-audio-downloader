be16570b61d8cd275bb3451ac1d0a1a4c1417914
bhwa233 <404174262@qq.com>
2025-06-13 09:45:33 +0800

    feat: integrate Vercel Analytics for enhanced tracking
    - Added `@vercel/analytics` dependency to `package.json` for improved analytics capabilities.
    - Integrated the `Analytics` component in `layout.tsx` to enable tracking across the application.
    - Updated `pnpm-lock.yaml` to include the new analytics package and its dependencies.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster"
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -67,6 +68,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
